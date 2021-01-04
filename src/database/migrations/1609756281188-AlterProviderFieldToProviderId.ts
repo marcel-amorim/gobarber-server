1147265f379128f929da0c43ebb9ddf3c4e4e6e3
Marcel Amorim <marcel_amorim@live.com>
2021-01-04 08:12:56 -0300

    Add Users and Change Appointments

---
+++ src/database/migrations/1609756281188-AlterProviderFieldToProviderId.ts
@@ -0,0 +1,42 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class AlterProviderFieldToProviderId1609756281188
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+      }),
+    );
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'AppointmentProvider',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+    await queryRunner.dropColumn('appointments', 'provider_id');
+    await queryRunner.addColumn(
+      'appointments',
+      new TableColumn({
+        name: 'provider',
+        type: 'uuid',
+      }),
+    );
+  }
+}
